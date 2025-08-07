f00579dbb879267f6ce9ba034a358b47f9e91205
Abdelrahman Tarek <abdelrhmant156@gmail.com>
2025-08-07 23:30:46 +0300

    last copy

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,33 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+// import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
+
 export class App {
-  protected readonly title = signal('To-do');
+  task = "";
+  taskList: { id: number, name: string, isDone: boolean }[] = []; 
+
+  addTask() {
+    if (this.task.trim()) { // Prevent adding empty tasks
+      this.taskList.push({id: this.taskList.length + 1, name: this.task,isDone: false });
+      this.task = ""; 
+    }
+  }
+
+  deleteTask(id: number) {
+    this.taskList = this.taskList.filter(task => task.id !== id);
+  }
+
+  doneTask(id: number) {
+    const task = this.taskList.find(task => task.id === id);
+  if (task) {
+    task.isDone = !task.isDone;
+  }
+  }
 }
